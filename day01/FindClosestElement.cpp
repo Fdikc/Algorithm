5aec280805236e0f9a9aa185157a2e20ee646519
fdikc <1924258102@qq.com>
2022-04-01 16:21:40 +0800

    master

--- day01/FindClosestElement.cpp
+++ day01/FindClosestElement.cpp
@@ -39,8 +39,9 @@ void FindCElement(int a[],int n,int m){
 }
 int main(int argc, char const *argv[])
 {
-    int a[100000],n,m;
+    int n,m;
     cin >> n;
+    int a[n];
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
